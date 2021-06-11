dbb91e96d3905bf98ca3e785f802878654289845
Tom <tohiggin@gmail.com>
2021-06-10 19:01:48 -0500

    Character Jump Fix
    Character controller now properly accounts for the fact that we can only jump once.

--- 2021Derust/Assets/Scripts/Character2DController.cs
+++ 2021Derust/Assets/Scripts/Character2DController.cs
@@ -7,10 +7,12 @@ public class Character2DController : MonoBehaviour
     [SerializeField] float MovementSpeed = 1;
     [SerializeField] float jumpForce = 1;
     private Rigidbody2D rigidbody;
+    private bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        isGrounded = true;
     }
 
     // Update is called once per frame
@@ -19,9 +21,31 @@ public class Character2DController : MonoBehaviour
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
         
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("Jump") && isGrounded == true)
         {
             rigidbody.AddForce(new Vector2(0, jumpForce));
+            isGrounded = false;
+            
         }
     }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void JumpInput()
+    {
+
+    }
+
+    private void GroundDetection()
+    {
+
+    }
+
 }
